2353f842b55af0a785eb1de81c0a00f370c939be
navneet-singh2907 <navnitgujjar@gmail.com>
2025-12-05 00:13:55 -0800

    fix: Refactor and stabilize toggle and delete logic using Event Delegation.

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const addTodoBtn = document.getElementById('addTodoBtn');
 const inputTag = document.getElementById('todoInput');
 const todoList = document.getElementById('todoList');
 
-let todoText; // Variable to store the todo text when add button is clicked
-
+let todoText; // Variable to store the todo text when add button is clicked 
 let todos = []; // Array to store todo items
 //if we have the todos in the local storage then we will read them
 let todoString = localStorage.getItem('todos');
@@ -13,66 +12,86 @@ if (todoString) {
 
 const populateTodos = () => {
     let string = "";
-    for (let item of todos) {
-        string += `<li id = "todo-${todo.id}" class="todo-item" ${item.isCompleted ? 'completed' : ''}>
-                    <input type="checkbox" class="todo-checkbox" ${item.isCompleted ? 'checked' : ''}/>
-                    <span class="todo-text">${item.title}</span>
+    for (const todo of todos) {
+
+        string += `<li id="todo-${todo.id}" class="todo-item ${todo.isCompleted ? 'completed' : ''}">
+                    <input type="checkbox" class="todo-checkbox" ${todo.isCompleted ? 'checked' : ''}/>
+                    <span class="todo-text">${todo.title}</span>
                     <button class="delete-btn">×</button>
                 </li>`
     }
     todoList.innerHTML = string;
+
+    //add checkbox logic to populated todos
+    const todoCheckboxes = document.querySelectorAll('.todo-checkbox');
+
+    todoCheckboxes.forEach((element) => {
+        element.addEventListener('click', (e) => {
+            if (e.target.checked) {
+                element.parentElement.classList.add('completed');
+
+
+                todos = todos.map((todo) => {
+                    if (("todo-" + todo.id) == element.parentNode.id) {
+                        return { ...todo, isCompleted: true }
+                    }
+                    else {
+                        return todo;
+                    }
+                });
+                localStorage.setItem('todos', JSON.stringify(todos));
+            }
+            else {
+                e.target.parentElement.classList.remove('completed');
+                todos = todos.map((todo) => {
+                    if (("todo-" + todo.id) == element.parentNode.id) {
+                        return { ...todo, isCompleted: false }
+                    }
+                    else {
+                        return todo;
+                    }
+                });
+                localStorage.setItem('todos', JSON.stringify(todos));
+            }
+
+        })
+
+    });
+    let deleteBtns = document.querySelectorAll('.delete-btn');
+    deleteBtns.forEach((element) => {
+        element.addEventListener('click', (e) => {
+            todos = todos.filter((todo) => {
+                return ("todo-" + todo.id) !== (e.target.parentNode.id);
+            });
+            localStorage.setItem('todos', JSON.stringify(todos));
+            populateTodos();
+
+        })
+    });
+
 }
+    addTodoBtn.addEventListener('click', () => {
+        
+        todoText = inputTag.value;
+        //chwck if the length of todo is greater than 3
+        if (todoText.trim().length < 3) {
+            alert("Todo text must be at least 3 characters long.");
+            return;
+        }   
+        inputTag.value = '';
 
-addTodoBtn.addEventListener('click', () => {
-    console.log('Hey I just clicked')
-    todoText = inputTag.value;
-    console.log(todoText);
-    inputTag.value = '';
+        let todo = {
+            id: "todo-" + Date.now(),
+            title: "" + todoText,
+            description: "This is a todo item",
 
-    let todo = {
-        id: todos.length,
-        title: "" + todoText,
-        description: "This is a todo item",
+            isCompleted: false
+        };
+        todos.push(todo);
 
-        isCompleted: false
-    };
-    todos.push(todo);
-    localStorage.setItem('todos', JSON.stringify(todos));
+        localStorage.setItem('todos', JSON.stringify(todos));
+        populateTodos();
+    });
     populateTodos();
-});
-populateTodos();
 
-const todoCheckboxes = document.querySelectorAll('.todo-checkbox');
 
-todoCheckboxes.forEach((element) => {
-    element.addEventListener('click', (e) => {
-        if (e.target.checked) {
-            e.target.parentElement.classList.add('completed');
-
-            
-            todos = todos.map((todo) => {
-                if (("todo-" + todo.id) === element.parentNode.id) {
-                    todo.isCompleted = true;
-                }
-                else {
-                    return todo;
-                }
-            });
-            localStorage.setItem('todos', JSON.stringify(todos));
-        }
-        else {
-            e.target.parentElement.classList.remove('completed');
-            todos = todos.map((todo) => {
-                if (("todo-" + todo.id) === element.parentNode.id) {
-                    todo.isCompleted = true;
-                }
-                else {
-                    return todo;
-                }
-            });
-            localStorage.setItem('todos', JSON.stringify(todos));
-        }
-
-    })
-
-});
